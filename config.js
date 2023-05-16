12f6d9bd55c8b3e85b0c145c96f5e7b4c8938509
Haroon000001 <132721083+Haroon000001@users.noreply.github.com>
2023-05-16 13:36:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['923155706147', 'sandaru', true],
+  ['923155706147', 'Haroon', true], 
   [''] 
 ] //Number of owners
 
